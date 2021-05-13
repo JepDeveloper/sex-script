76004878bde789bd5feed40f14fff9f165c37c12
iCyphy <72982537+iCyphyDev@users.noreply.github.com>
2021-05-13 14:12:10 +0530

    Create idk.lua

---
+++ idk.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 2041312716 then  --ragdoll engine suckers
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+   local Window = Library.CreateLib("Sex", "BloodTheme")
+ end   
